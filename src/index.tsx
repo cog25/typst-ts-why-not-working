921bccd757aad08b3638788619229e569979dccb
cog25 <74242561+cog25@users.noreply.github.com>
2024-12-28 00:44:29 +0900

    refactor: update Typst renderer and compiler imports; add font preloading

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,27 @@ import { render } from 'solid-js/web'
 
 import { createTypstCompiler } from "@myriaddreamin/typst.ts/dist/esm/compiler"
 import { onMount } from 'solid-js'
-import { createTypstRenderer } from '@myriaddreamin/typst.ts/dist/cjs/renderer.cjs'
+import { createTypstRenderer } from '@myriaddreamin/typst.ts/dist/esm/renderer'
+import { preloadRemoteFonts } from '@myriaddreamin/typst.ts/dist/esm/options.init'
 
 const App = () => {
 
     onMount(async () => {
-        const cc = createTypstCompiler();
-        await cc.init({ getModule: () => `https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler@${__TYPST_TS_VERSION__}/pkg/typst_ts_web_compiler_bg.wasm` })
 
+        const cc = createTypstCompiler();
+        await cc.init({
+            getModule: () => `https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-web-compiler@${__TYPST_TS_VERSION__}/pkg/typst_ts_web_compiler_bg.wasm`,
+            beforeBuild: [
+                preloadRemoteFonts([
+                    // Some Korean Font.
+                    "https://cdn.jsdelivr.net/gh/fonts-archive/Freesentation/Freesentation-5Medium.ttf"
+                ])
+            ]
+        });
+
+
+        cc.addSource("/main.typ", "Hello.");
         const { result } = await cc.withIncrementalServer(async (incrServer) => {
-            cc.addSource("/main.typ", "Hello.");
             return cc.compile({
                 diagnostics: "none",
                 incrementalServer: incrServer,
@@ -22,11 +33,11 @@ const App = () => {
         });
 
         console.log(!!result);
-        if(!result) return;
+        if (!result) return;
 
         const renderer = createTypstRenderer()
-        await renderer.init( { getModule: () => `https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer@${__TYPST_TS_VERSION__}/pkg/typst_ts_renderer_bg.wasm` })
-        const svg = await renderer.runWithSession(async (kModule)=>{
+        await renderer.init({ getModule: () => `https://cdn.jsdelivr.net/npm/@myriaddreamin/typst-ts-renderer@${__TYPST_TS_VERSION__}/pkg/typst_ts_renderer_bg.wasm` })
+        const svg = await renderer.runWithSession(async (kModule) => {
             return renderer.renderSvg({
                 artifactContent: result,
                 format: "vector",
